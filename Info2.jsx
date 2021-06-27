9af9bf6dcff24197a8a4e4ae5e0ed94e02a9d8d1
Kavyasree1903 <69076343+Kavyasree1903@users.noreply.github.com>
2021-06-28 00:32:44 +0530

    Add files via upload

---
+++ Info2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info2() {
+  return (
+    <div class="note">
+      <h1>Python and Network Security</h1>
+      <p>Its a basic bootcamp on python and network security</p>
+    </div>
+  );
+}
+
+export default Info2;
